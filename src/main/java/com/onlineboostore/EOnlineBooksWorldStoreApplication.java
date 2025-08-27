b80f12f43e56afdf9cfe40103fb476d40da8fed0
rohitlavate97 <rohitlavate7@gmail.com>
2025-08-27 22:47:45 +0530

    Initial commit

---
+++ src/main/java/com/onlineboostore/EOnlineBooksWorldStoreApplication.java
@@ -0,0 +1,13 @@
+package com.onlineboostore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EOnlineBooksWorldStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EOnlineBooksWorldStoreApplication.class, args);
+    }
+
+}
